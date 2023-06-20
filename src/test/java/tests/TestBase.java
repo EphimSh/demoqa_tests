da7074a38d86a92a45b9ec190e4f266cbbea9a53
EphimSh <ephimizm@gmail.com>
2023-06-20 20:45:17 +0300

    TestBase updated

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
+    Faker faker = new Faker();
 
     @BeforeAll
     static void init(){
